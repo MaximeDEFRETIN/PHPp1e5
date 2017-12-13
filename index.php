ed9bafff5b9253a528d017484d627abdf1b52e15
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-12-13 08:40:44 +0100

    Correction

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<?php
-$int;
-echo $int;
-$int = 15.17;
-echo $int;
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Partiee 1 : exercice 5</title>
+  </head>
+  <body>
+    <?php
+    settype($int, 'integer');
+    $int = null;
+    echo $int;
+    $int = 15;
+    echo $int;
+    ?>
+  </body>
+</html>
